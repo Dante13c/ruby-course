4a21ae121a0044d4e41b5253beb270d72e409061
Apollo <dantelanders113@yahoo.com>
2025-09-01 00:19:18 -0500

    Elsif

---
+++ 2.Control Flow in Ruby/4.Elsif.rb
@@ -0,0 +1,11 @@
+name = "Dante"
+name = "Claere"
+name = "Alex"
+
+if name == "Dante"
+  puts "Welcome back, #{name}!"
+elsif name == "Claere"
+  puts "Hello, #{name}! Long time no see!"
+else
+  puts "Hello, #{name}! I don't think we've met."
+end
