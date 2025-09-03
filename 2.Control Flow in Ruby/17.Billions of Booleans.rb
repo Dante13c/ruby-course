cc3703b0add8e03b522944a7c6e6d01c7aae7bb5
Apollo <dantelanders113@yahoo.com>
2025-09-02 19:55:01 -0500

    Billions of Booleans

---
+++ 2.Control Flow in Ruby/17.Billions of Booleans.rb
@@ -0,0 +1,12 @@
+# test_1 should be true
+test_1 = (2 * 5) == 10 && (3 + 7) == 10
+
+# test_2 = should be true
+test_2 =  (10 / 2) == 5 || false
+
+# test_3 = should be false
+test_3 = !true 
+
+puts test_1
+puts test_2
+puts test_3
