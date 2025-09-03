220e51a83728be66ae8d75f55c961af28b5ec50e
Apollo <dantelanders113@yahoo.com>
2025-09-02 19:35:33 -0500

    Dare to Compare

---
+++ 2.Control Flow in Ruby/16.Dare to Compare.rb
@@ -0,0 +1,12 @@
+# test_1 should be false
+test_1 = 1 > 2
+
+# test_2 = should be false
+test_2 = 2**3 == 3**2
+
+# test_3 = should be true
+test_3 = 5**2 == 25
+
+puts test_1
+puts test_2
+puts test_3
