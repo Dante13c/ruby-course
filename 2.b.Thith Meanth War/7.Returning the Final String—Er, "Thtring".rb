6606234c0804dbcb8933177625b7f8ee3d28ca23
Dante13c <100056438+Dante13c@users.noreply.github.com>
2025-09-06 01:44:22 -0500

    print "Pleathe enter a thtring: "  user_input = gets.chomp user_input.downcase!  if user_input.include? "s"     user_input.gsub!(/s/, "th")     print user_input else     print "there is no s in the sting"     puts "your string says: #{user_input}!" end

---
+++ 2.b.Thith Meanth War/7.Returning the Final String—Er, "Thtring".rb
@@ -0,0 +1,11 @@
+print "Pleathe enter a thtring: " 
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+    user_input.gsub!(/s/, "th")
+    print user_input
+else
+    print "there is no s in the sting"
+    puts "your string says: #{user_input}!"
+end
