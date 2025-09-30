f6db8a3a248ca5ceb1701725c703dfe2a9413528
Kevin Jarrett <dantelanders113@yahoo.com>
2025-09-30 12:16:25 -0500

    Project completed: Passed

---
+++ 3.Loops & Iterators/Redacted!/5.Control Flow Know-How.rb
@@ -0,0 +1,31 @@
+# User Inputs 
+puts "you will enter two inputs now "
+
+puts "please enter first input"         # Enter Input for first variable 
+text = gets.chomp         
+
+puts "please eneter second input"         #Enter input for second variable
+redact = gets.chomp
+
+# Control Flow Logic
+if text.length == 0 && redact.length == 0       #Checks both variable if empty
+  text = "default Text"                                                     #\_____Replaces null for default text 
+  redact = "default redact"
+elsif text.length == 0 && redact.length != 0        #Checks first variable if empty and mimics behavior of the code above 
+  text = "default missing text"
+elsif text.length != 0 && redact.length == 0        #checks second variable if empty
+  redact = "default missing redact"
+end
+
+words = text.split(" ")
+
+
+#Output Control
+words.each do |word|
+  if word == redact         # Filter for redacting words; replaces word with string "REDACTED"
+    print "REDACTED "
+  else 
+    print print word + " "
+  end
+end
+
