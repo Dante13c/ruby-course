1e2a7f6c4b4bc5cf4a1b677eaf4db1c7995208fb
Kevin Jarrett <dantelanders113@yahoo.com>
2025-09-30 11:31:07 -0500

    Project files and souurce code defined

--- 3.Loops & Iterators/Redacted!/1.What You'll Be Building.rb
+++ 3.Loops & Iterators/Redacted!/1.What You'll Be Building.rb
@@ -1,16 +1,23 @@
+# User Inputs 
 puts "you will enter two inputs now "
 
-puts "please enter first input"
-text = gets.chomp
+puts "please enter first input"         # Enter Input for first variable 
+text = gets.chomp         
 
-puts "please eneter second input"
+puts "please eneter second input"         #Enter input for second variable
 redact = gets.chomp
 
-if text.length == 0 && redact.length == 0
-  text = "default Text"
+# Control Flow Logic
+if text.length == 0 && redact.length == 0       #Checks both variable if empty
+  text = "default Text"                                                     #\_____Replaces null for default text 
   redact = "default redact"
+elsif text.length == 0 && redact.length != 0        #Checks first variable if empty and mimics behavior of the code above 
+  text = "default missing text"
+elsif text.length != 0 && redact.length == 0        #checks second variable if empty
+  redact = "default missing redact"
 end
 
-puts "your text: #{text}"
-puts "your redact: #{redact}"
+#View Logic 
+puts "your text: #{text}"               #Prints to console
+puts "your redact: #{redact}" 
 
